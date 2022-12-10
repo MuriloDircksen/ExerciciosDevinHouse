c48297a419fb33aa155f7d52e8d25b4ce5f95c52
Murilod <murilo.dircksen@gmail.com>
2022-12-10 09:16:40 -0300

    exercicio 2

--- Semana2/index.js
+++ Semana2/index.js
@@ -1,12 +1,25 @@
-var idade = parseInt(prompt("Qual sua idade"))
+function exec1() {
+    var idade = parseInt(prompt("Qual sua idade"))
 
-if(idade>=0 && idade <= 15){
-    alert("Jovem")
-}else if(idade >15 && idade <=64){
-    alert("Adulto")
-}else if(idade>64){
-    alert("Idosos")
+    if (idade >= 0 && idade <= 15) {
+        alert("Jovem")
+    } else if (idade > 15 && idade <= 64) {
+        alert("Adulto")
+    } else if (idade > 64) {
+        alert("Idosos")
+    }
+    else {
+        alert("Dado inválido inserido")
+    }
 }
-else{
-    alert("Dado inválido inserido")
+
+function exec2(){
+    var numero = parseInt(prompt("DIgite número para resolver a tabuada?"))
+    var resultado = '';
+    if(!isNaN(numero)){
+        for(i=0; i<=10; i++){
+            resultado = resultado + `${numero} x ${i} = ${numero*i} \n`
+        }
+    }
+    alert(resultado)
 }
