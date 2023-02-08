2587a94a81a8ede9609305a840dd4236dc25b129
Murilod <murilo.dircksen@gmail.com>
2023-02-08 16:38:20 -0300

    Exercicio 2 semana 6

--- Semana 6/app.js
+++ Semana 6/app.js
@@ -12,3 +12,22 @@ function sleep ( valor ) {
     })
   }
 sleep("teste")
+
+//exercicio 2
+
+console.log("Exercicio 2")
+
+const repetir = setInterval(() => {
+
+    const data = new Date(); // momento atual 
+    const horas = data.getHours();
+    const minutos = data.getMinutes();
+    const segundos = data.getSeconds();
+
+    const hhmmmss = [horas, minutos, segundos].join(':');
+
+    console.log(hhmmmss)
+
+},1000);
+
+setTimeout(() => clearInterval(repetir), 10000);
