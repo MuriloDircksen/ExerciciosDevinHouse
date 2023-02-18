353f2094813a9f4beb1cefec7549892bc76d872a
Murilod <murilo.dircksen@gmail.com>
2023-02-18 10:57:14 -0300

    exercicio completo com correções semana 7

--- Semana 7/Exemplo Angular/teste_angular/src/component/galeria/galeria.component.ts
+++ Semana 7/Exemplo Angular/teste_angular/src/component/galeria/galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class GaleriaComponent {
 
+  public titulo : string = "Galeria de fotos"
+  public fotoAtual = 0;
+  @Input() public fotos : string[] = [];
+
+
+
+  public voltarParaPrimeira(){
+    this.fotoAtual = this.primeira();
+  }
+  public vaiParaProxima(){
+    this.fotoAtual++;
+  }
+  public vaiParaUltima(){
+    this.fotoAtual = this.ultima();
+  }
+  public voltarParaAnterior(){
+    this.fotoAtual--;
+  }
+  public primeira(){
+    return 0;
+  }
+  public ultima(){
+    return this.fotos.length - 1;
+  }
 }
