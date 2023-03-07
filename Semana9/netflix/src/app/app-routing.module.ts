cc698685fde0979031882e63d93ed49c409cef83
Murilod <murilo.dircksen@gmail.com>
2023-03-07 10:15:06 -0300

    Ex 6 - Criando proteção de rotas do fluxo após login

--- Semana9/netflix/src/app/app-routing.module.ts
+++ Semana9/netflix/src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
+import { SerieComponent } from './components/serie/serie.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { AuthGuard } from './components/security/guard/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: MenuComponent,
+    canActivate:[AuthGuard],
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'series/:id',
+        component: SerieComponent
+      }
+
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
