45ae47c02bffd64f363926a18b72eae51127a217
Murilod <murilo.dircksen@gmail.com>
2023-03-12 14:19:46 -0300

    Ex 2 - Desenvolva dois layouts para receber um modelo de dados de tela com menu e outra sem menu

--- Semana10/lojinha/src/app/app-routing.module.ts
+++ Semana10/lojinha/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
+import { FullComponent } from './layouts/full/full.component';
+import { ContentComponent } from './layouts/content/content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentComponent,
+    children:[
+      {path: '',
+      redirectTo: 'login',
+      pathMatch: 'full'
+      },
+      {
+        path: 'login',
+
+      }
+    ]
+  },
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
